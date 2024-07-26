f89adac464677b92507b1e243a7d79a1e163227e
magnus2024 <osagieokitikpi@gmail.com>
2024-07-26 10:41:46 +0100

    Peter

---
+++ Osas folder/osas.py
@@ -0,0 +1,15 @@
+# first_name=input("Enter your first name:")
+# last_name=input("Enter your last name:")
+# age=input("Enter your age:")
+# phone_number=input("Enter your phone number:")
+# print(f"My name is {first_name} {last_name} and I am {age} years old. You can contact me on {phone_number}.")
+
+# numb1=int("10")
+# numb2=int("15")
+# numb3=int("25")
+# print(f"{numb1} + {numb2} + {numb3}={numb1 + numb2 + numb3}")
+
+
+
+
+
---
+++ lesson/lesson 1.py
@@ -0,0 +1,301 @@
+#1
+#name=input("What is your name:")
+#print(f"You are welcome Mr.{name}.")
+
+#2
+#x =10
+#y =15
+#print(f"{x} + {y} = {x + y}")
+
+#3
+# first_name=input("Enter your first name:")
+# last_name=input("Enter your last name:")
+# age=25
+# print(f"You are welcome Mr. {first_name} {last_name} and you are {age} years old.")
+
+#4
+# mycolour=input("Enter your favourite colour:")
+# animal=input("Enter your favourite animal:")
+# print(f"My favourite colour is {mycolour} and my faovurite animal is {animal}.")
+
+#5
+# first_name=("John")
+# current_year=2024
+# birth_year=2000
+# print(f"My name is {first_name} and I was born in the year {birth_year}. I am {current_year} - {birth_year} = {current_year - birth_year} years old.")
+
+#6a
+# town=input("Enter your home town:")
+# food=input("What is your favourite food:")
+# print(f"My home town is {town} and my favourite food is {food}")
+
+#6b
+# title=input('Enter title:')
+# author=input('Enter the Author:')
+# print(f'The title of the book is {title} and the author is {author}.')
+
+#7
+# city_name=input("Enter city visited:")
+# year=input("Enter the year visited:")
+# print(f"I visited {city_name} in {year}.")
+
+#8
+# movie=("The goods must be crazy")
+# year=1980
+# print(f"My favourite move is called {movie} and it was release in {year}.")
+
+#9
+# fruit1=input('Enter your favourite fruit:')
+# fruit2=input('Enter your favourite fruit:')
+# fruit3=input('Enter your favourite fruit:')
+# week1=input("twice a week")
+# week2=input("thrice a week")
+# week3=input("once a week")
+# week1="twice"
+# week2="thrice"
+# week3="once"
+# print(f"I eat {fruit1} {week1} a week, {fruit2} {week2} a week and {fruit3} {week3} a week.")
+
+#10
+# subject=input("Enter your favour subject:")
+# grade_level=input("Enter your grade:")
+# print(f"My favourite subject is {subject} and my grade level is {grade_level}.")
+
+#11
+# sport=input("Enter your favourite sport:")
+# season1=input("which season do you play it:")
+# season=("summer")
+# print(f"My favourite sport is {sport} and it is played during {season1}.")
+
+
+#1. Creating an empty list:
+#To create an empty list in Python, you can use:
+#empty_list = []
+#2. Accessing the third element in a list:
+#If 'my_list' is your list, you can access the third element (index 2) using:
+#my_list = []
+#third_element = my_list[2]
+#3. Adding an element 'apple' to the end of a list named fruits:
+#You can add 'apple' to the end of the `fruits` list using the `append()` method:
+##fruits = []
+#fruits.append('apple')
+#4. Removing the third element from a list named numbers:
+# To remove the third element (index 2) from 'numbers' , you can use 'pop()':
+#numbers = []
+###numbers.pop(2)
+#5. Difference between 'append()' and 'extend()' methods:
+#'append()' adds its argument as a single element to the end of a list.
+#'extend()' iterates over its argument adding each element to the list, extending the list.
+#6. Checking if 'banana' exists in a list named fruits:**
+#To check if 'banana' exists in `fruits`, you can use the `in` keyword:
+#fruits = []
+#if 'banana' in fruits : print("Yes, 'banana' is in the list.")
+#7. Output of `len(['a', 'b', 'c'])`:
+#The output will be:3
+#8. Reversing the order of elements in a list named my_list:
+#You can reverse `my_list` using slicing:
+##reversed_list = my_list[::-1]
+#9. Purpose of the `sort()` method in Python lists:
+#The `sort()` method sorts the elements of a list in place, i.e., it modifies the original list to be in ascending order.
+#10.
+#11. String in Python:
+#A string in Python is a sequence of characters enclosed within quotes (single `'` or double `"`).
+#12. Creating an empty string:**
+#You can create an empty string by assigning an empty pair of quotes:
+#empty_string = ""
+#13. Accessing characters in a string using indexing:
+#Yes, you can access characters in a string using square brackets and an index:
+#my_string = ""
+#char = my_string['index']
+#14. Finding the length of a string:**
+#You can find the length of a string using the `len()` function:
+#length = len(my_string)
+#15. Concatenating two strings:
+#Strings can be concatenated using the `+` operator:
+#concatenated_string = string1 + string2
+#string_1 = ()
+#string_2 = ()
+#print(string_1 + string_2 )
+#16. Converting a string to uppercase:
+#Use the `upper()` method:
+#uppercase_string = my_string.upper()
+#print(my_string.upper)
+#17. Converting a string to lowercase:
+#Use the `lower()` method:
+#lowercase_string = my_string.lower()
+#print(my_string.lower)
+#18. Checking if a substring exists in a string:
+#Use the `in` keyword:
+#if 'substring' in my_string:print("Substring found.")
+#19. Making a string into a list of substrings:
+#Use the `split()` method:
+#substrings_list = my_string.split()
+#20. Replacing occurrences of a substring within a string:
+#Use the `replace()` method:
+##new_string = my_string.replace('old_substring', 'new_substring')
+#21. Finding the index of a substring within a string:
+#Use the `find()` method or `index()` method:
+#index = my_string.find('substring')
+#22. Iterating over the characters of a string:
+#You can iterate over characters in a string using a for loop:
+#for char in my_string:
+#print(char)
+#23. Reversing a string in Python:
+#You can reverse a string using slicing:
+#reversed_string = my_string[::-1]
+#24. Converting an integer or float to a string:
+#Use the `str()` function:
+#number = ()
+#num_str = str(number)
+#25. Checking if a string contains only digits:
+#Use the `isdigit()` method:
+#if my_string.isdigit():print("String contains only digits.")
+#26. Finding the sum of all elements in a list of numbers:
+#You can use the `sum()` function:
+#total_sum = sum(numbers_list)
+#27. Accessing the third element of a list:
+#Use indexing:
+#third_element = my_list[2]
+#28. Finding the maximum and minimum elements in a list of numbers:
+#Use the `max()` and `min()` functions:
+#max_element = max(numbers_list)
+#min_element = min(numbers_list)
+#29. List comprehension in Python:
+#List comprehension provides a concise way to create lists:
+#squares = [x**2 for x in range(10)]
+#30. Checking if a specific element exists in a list:**
+#Use the `in` keyword:
+#if element in my_list:print("Element found.")
+#31. Reversing a list:
+#You can reverse a list using slicing:
+#reversed_list = my_list[::-1]
+
+#FORMULAS
+# print(fruit)
+# number = [20, 40, 50, 32, 49, 25]
+# for david in number:
+#     print(david ** 2)
+# my_strings = str()
+# print(len(my_strings))
+# print(type(my_strings))
+# string1 = 'John is a python developer'
+# y = string1.split()
+# y.reverse()
+# print(y)
+# for i in y:
+#     if i[0] == 'J':
+#         print(i)
+#     else:
+#         print('False')
+# print(string1.split())
+# print(string1.replace('python', 'Javascripts'))
+# print(string1.find('python'))
+# print(string1.reverse())
+# x = string1
+# print(x[::-1])
+# print(string1.lower())
+# print(string1.endswith('n'))
+# print(string1.startswith('Z'))
+# string2 = 'Doe'
+# print(f'{string1} {string2}')
+# 10:33
+# print(fruit)
+# number = [20, 40, 50, 32, 49, 25]
+# for david in number:
+#     print(david ** 2)
+# my_strings = str()
+# print(len(my_strings))
+# print(type(my_strings))
+# string1 = 'John is a python developer'
+# y = string1.split()
+# y.reverse()
+# print(y)
+# for i in y:
+#     if i[0] == 'J':
+#         print(i)
+#     else:
+#         print('False')
+# print(string1.split())
+# print(string1.replace('python', 'Javascripts'))
+# print(string1.find('python'))
+# print(string1.reverse())
+# x = string1
+# print(x[::-1])
+# print(string1.lower())
+# print(string1.endswith('n'))
+# print(string1.startswith('Z'))
+# string2 = 'Doe'
+# print(f'{string1} {string2}')
+
+#QUESTIION
+#my_var = [10, 30, ['digital', ['fortress', 'software devgelopment',['data analysis', ['mango'], 'banana'], 'water melon'], 'frontend'], 50, 70, 90]
+
+#1
+#print out data analysis
+#print(my_var[2][1][2][0])
+#2
+# print out the number 90
+#print(my_var[5])
+#3
+# print out the word digital
+#print(my_var[2][0])
+#4
+# print out water melon
+#print(my_var[2][1][3])
+#5
+# print out frontend
+#print(my_var[2][2])
+
+#my_data = [
+#'first_name':'John',
+#'last_name': 'Doe',
+#'country': 'USA',
+#'LGA': 'Washington Dc'},
+#'first_name':'David',
+#'last_name': 'Desmond',
+#'country': 'Kenyan',
+#'LGA': 'Naira Obi'
+#},
+#{'first_name':'Tosin'}
+#'last_name': 'Adeleke',
+#'country': 'Nigeria',
+#'LGA': 'Lagos State'},
+##'first_name':'Emeka',
+##'last_name': 'Prosper',
+##'country': 'Nigeria',"USA", "GERMANY"
+#'LGA': 'Abia'}]
+#print(my_data[0]['first_name'][2]('country'))
+#my_data=[0]['first_name']='tunde'
+##print(my_data)
+
+#Assignment
+#Write out all the first name on the data.
+
+#DATA
+#my-data
+
+#NUMBER
+num=[20, 50, 10, 40, 55, 4,8]
+#x=[y for y in num if y > 10]
+#print(x)
+#num= 10
+
+#Dictionary
+#my_dict = ['first_name', 'tunde']
+
+#BOOLEAN
+ #name = False
+ #print(type(name))
+# dictionary
+#employee =  {'first_name':'Emeka',
+ #    'last_name': 'Prosper',
+  #   'country': 'Nigeria',
+   #  'LGA': 'Abia'
+    # }
+# print(employee.values())
+#employee['country']='USA'
+#employee['occupation'] = " Web Developer"
+#employee.update({'first_name': 'Tunde'})
+#employee.update({'salary': 100000})
+#print(employee)
+
